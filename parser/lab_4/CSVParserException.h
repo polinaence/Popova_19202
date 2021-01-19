4f274240da10bb4ef5d86b3930887af20492583d
polinaence <72161124+polinaence@users.noreply.github.com>
2021-01-19 15:05:18 +0700

    Add files via upload

---
+++ parser/lab_4/CSVParserException.h
@@ -0,0 +1,49 @@
+//
+// Created by Anna on 07.01.2021.
+//
+
+#ifndef LAB_4_CSVPARSEREXCEPTION_H
+#define LAB_4_CSVPARSEREXCEPTION_H
+
+#include <exception>
+#include <string>
+
+namespace CSVParser {
+
+    enum class ExceptionType {
+        InvalidData,
+        DataOverflow,
+        DataUnderflow
+    };
+
+    class CSVParserException : public std::exception {
+
+    private:
+        ExceptionType errorType_;
+        std::string errorMessage_;
+        unsigned int currentErrorColumn_ = 0;
+    public:
+        CSVParserException() = default;
+
+        explicit CSVParserException(const std::string &errorMessage, const unsigned int column, ExceptionType type) {
+            errorMessage_ = errorMessage;
+            currentErrorColumn_ = column;
+            errorType_ = type;
+        }
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return errorMessage_.c_str();
+        }
+
+        [[nodiscard]] unsigned int getErrorColumn() const noexcept {
+            return currentErrorColumn_;
+        }
+
+        [[nodiscard]] ExceptionType getErrorType() const noexcept {
+            return errorType_;
+        }
+    };
+
+}
+
+#endif //LAB_4_CSVPARSEREXCEPTION_H
---
+++ parser/lab_4/TemplateTuplePrinter.h
@@ -0,0 +1,53 @@
+//
+// Created by Anna on 07.01.2021.
+//
+
+#ifndef OOP_FIT_C_TEMPLATETUPLEPRINTER_H
+#define OOP_FIT_C_TEMPLATETUPLEPRINTER_H
+
+#include <cstdio>
+#include <tuple>
+#include <iostream>
+
+namespace TupleOperators {
+
+    static const char LEFT_BORDER_SYMBOL = '[';
+    static const char RIGHT_BORDER_SYMBOL = ']';
+    static const char TOKENS_DELIMITER = ' ';
+
+    template<typename Ch, typename Tr, class Tuple, size_t N>
+    class TuplePrinter {
+    public:
+        /*
+         * Recursive printing std::tuple with N elements
+         */
+        static void print(std::basic_ostream<Ch, Tr> &out, const Tuple &t) {
+            TuplePrinter<Ch, Tr, Tuple, N - 1>::print(out, t);
+            out << TOKENS_DELIMITER << std::get<N - 1>(t);
+        }
+    };
+
+    /*
+     * Class-stopping print template std::tuple recursion
+     */
+    template<typename Ch, typename Tr, class Tuple>
+    class TuplePrinter<Ch, Tr, Tuple, 1> {
+    public:
+        static void print(std::basic_ostream<Ch, Tr> &out, const Tuple &t) {
+            out << std::get<0>(t);
+        }
+    };
+
+    template<typename Ch, typename Tr, typename... Args>
+    auto &operator<<(std::basic_ostream<Ch, Tr> &out, const std::tuple<Args...> &tuple) {
+        out << LEFT_BORDER_SYMBOL;
+        TuplePrinter<Ch, Tr, decltype(tuple), sizeof...(Args)>::print(out, tuple);
+        out << RIGHT_BORDER_SYMBOL;
+
+        return out;
+    }
+
+}
+
+
+#endif //OOP_FIT_C_TEMPLATETUPLEPRINTER_H
